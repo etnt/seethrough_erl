c007c77a80cfb3b1bfd8494263fd27ebdb0c1d3d
Torbjorn Tornkvist <tobbe@tornkvist.org>
2009-04-05 11:17:45 +0200

    Adding initial XML Schema for Nitrogen.

---
+++ test/nitrogen_test.erl
@@ -0,0 +1,14 @@
+-module(nitrogen_test).
+
+-include_lib("xmerl/include/xmerl.hrl").
+-include_lib("eunit/include/eunit.hrl").
+-compile([export_all]).
+
+
+wire_test() ->
+    {E,_} = xmerl_scan:file("./priv/wire.xml"),
+    {ok,S} = xmerl_xsd:process_schema("priv/nitrogen.xsd",
+                                      [{xsdbase,"."}]),
+    ?assertMatch({#xmlElement{},_}, xmerl_xsd:validate(E,S)).
+
+
